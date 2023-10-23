b424c32c84e91eb394abeba186f6059333aba6f1
BenAr99 <getto_Tawers@mail.ru>
2023-10-23 20:49:51 +0400

    refactor: fix eslint

--- youtube-client-app/src/app/modules/video/video.module.ts
+++ youtube-client-app/src/app/modules/video/video.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoPreviewComponent } from './components/video-preview/video-preview.component';
 import { VideoRatingListComponent } from './components/video-rating-list/video-rating-list.component';
+import { VideoPreviewListComponent } from './components/video-preview-list/video-preview-list.component';
 
 @NgModule({
-  declarations: [VideoPreviewComponent, VideoRatingListComponent, VideoRatingListComponent],
+  declarations: [VideoPreviewComponent, VideoPreviewListComponent, VideoRatingListComponent],
   imports: [
     CommonModule,
   ],
